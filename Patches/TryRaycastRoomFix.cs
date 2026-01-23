718cd17e32638b1dddb49671b882647454ceab09
TheLongQuite <okilov.rus@gmail.com>
2026-01-23 08:34:13 +0500

    Фикс невероятно редкого бага с боеголовкой, оно же нам надо, доооо

---
+++ Patches/TryRaycastRoomFix.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using Interactables.Interobjects;
+using MapGeneration;
+using ProjectMER.Features.Objects;
+using UnityEngine;
+using Object = System.Object;
+
+namespace ProjectMER.Patches;
+
+[HarmonyPatch(typeof(RoomUtils), nameof(RoomUtils.TryRaycastRoom))]
+public class TryRaycastRoomFix
+{
+    public static bool Prefix(Vector3 pos, Vector3 dir, ref bool __result, out RoomIdentifier room)
+    {
+        if (Physics.Raycast(new Ray(pos, dir), out RaycastHit hitInfo, 
+                15f, (int) RoomUtils.RoomDetectionMask) && hitInfo.collider != (Object) null)
+        {
+            Transform transform = hitInfo.collider.transform;
+            if (transform.TryGetComponentInParent(out room))
+            {
+                __result = true;
+                return false;   
+            }
+
+            if (transform.TryGetComponentInParent(out MapEditorObject mapEditorObject) && mapEditorObject.CurrentRoom)
+            {
+                room = mapEditorObject.CurrentRoom.Identifier;
+                __result = true;
+                return false;    
+            }
+
+            if (transform.TryGetComponentInParent(out ElevatorChamber comp))
+            {
+                room = comp.CurrentRoom;
+                __result = room != (Object) null;
+                return false;
+            }
+        }
+        
+        room = null;
+        __result = false;
+        return false;    
+    }
+}
