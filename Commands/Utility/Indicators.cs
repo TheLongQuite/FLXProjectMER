494746f700f0c2e460e29eff631b383237d329ba
JaLawash <greenmag305@gmail.com>
2026-07-31 16:26:51 +0200

    fff

--- Commands/Utility/Indicators.cs
+++ Commands/Utility/Indicators.cs
@@ -14,9 +14,9 @@ public class Indicators : ICommand
 
     public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
     {
-        if (!sender.CheckPermission($"mpr.{Command}"))
+        if (!sender.CheckPermission("mpr.indicators"))
         {
-            response = $"You don't have permission to execute this command. Required permission: mpr.{Command}";
+            response = $"You don't have permission to execute this command. Required permission: mpr.indicators";
             return false;
         }
 
